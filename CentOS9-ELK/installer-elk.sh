4f4dbddd8b40277d99f7db4c58faf379ef1f195e
Leslie <lesysec@gmail.com>
2022-12-20 10:53:37 +0900

    Fix elk installation error

--- CentOS9-ELK/installer-elk.sh
+++ CentOS9-ELK/installer-elk.sh
@@ -4,27 +4,24 @@
 ## Ready to run
 sudo -Sv
 ORGNPATH=$(pwd)
-sudo dnf install -y java-17-openjdk java-17-openjdk-devel
+sudo dnf install -y firewalld
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
 
-sudo firewall-cmd --permanent --zone=public --add-port=9200/tcp
-sudo firewall-cmd --permanent --zone=public --add-port=5601/tcp
-sudo firewall-cmd --reload
-
-## Install Elasticsearch, Logstash and Kibana
+## Get Elasticsearch, Logstash and Kibana repo
 sudo update-crypto-policies --set LEGACY
 sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 sudo cp "$ORGNPATH"/cfg/elasticsearch8.repo /etc/yum.repos.d/elasticsearch.repo
 sudo cp "$ORGNPATH"/cfg/logstash8.repo /etc/yum.repos.d/logstash.repo
 sudo cp "$ORGNPATH"/cfg/kibana8.repo /etc/yum.repos.d/kibana.repo
 
+## Install Elasticsearch, Logstash and Kibana
 sudo dnf --enablerepo=elasticsearch install -y elasticsearch | tee elastic-install.log
-sudo systemctl enable elasticsearch
-sudo systemctl start elasticsearch
-
 sudo dnf --enablerepo=elasticsearch install -y logstash
-sudo systemctl enable logstash
-sudo systemctl start logstash
-
 sudo dnf --enablerepo=elasticsearch install -y kibana
+sudo systemctl enable elasticsearch
+sudo systemctl enable logstash
 sudo systemctl enable kibana
+sudo systemctl start elasticsearch
+sudo systemctl start logstash
 sudo systemctl start kibana
--- UbuntuLTS-ELK/installer-elk.sh
+++ UbuntuLTS-ELK/installer-elk.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 # Ubuntu LTS with ELK Installer
 
+## Ready to run
+sudo -Sv
+ORGNPATH=$(pwd)
 sudo apt install -y ufw
+sudo ssh allow ssh
 sudo ufw enable
 
-curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+## Get Elasticsearch, Logstash and Kibana repo
 sudo apt install -y apt-transport-https
-
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-echo "deb https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
-echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-
 sudo apt update
+
+## Install Elasticsearch, Logstash and Kibana
 sudo apt install -y elasticsearch | tee elastic-install.log
 sudo apt install -y logstash
 sudo apt install -y kibana
+
+sudo mv /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.bck
+sudo mv /etc/kibana/kibana.yml /etc/kibana/kibana.yml.bck
+sudo cp "$ORGNPATH"/cfg/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp "$ORGNPATH"/cfg/kibana.yml /etc/kibana/kibana.yml
 
 sudo systemctl enable elasticsearch
 sudo systemctl enable logstash
@@ -23,6 +31,11 @@ sudo systemctl start elasticsearch
 sudo systemctl start logstash
 sudo systemctl start kibana
 
+## Configure Port
 #sudo ufw allow from <ALLOW IP> to any port 9200
-sudo /usr/share/elasticsearch/bin/kibana-verification-code
+#sudo ufw allow from <ALLOW IP> to any port 5601
+
+## Kibana Installation func
+sudo /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token --scope kibana
+sudo /usr/share/kibana/bin/kibana-verification-code
 echo "-----DONE!-----"
